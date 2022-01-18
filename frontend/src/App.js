4620b9879141f22ec132a4baf41700dff199d32a
SrivedaAnnaparthi <sriveda.annaparthi@arshaa.com>
2022-01-18 16:05:12 +0530

    Integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import PrimarySearchAppBar from './Pages/HomePage';
+// import PrimarySearchAppBar from './Pages/HomePage';
 import { Route ,Routes} from 'react-router-dom';
 import Form from './Pages/Form';
+import MiniDrawer from'./Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
 function App() {
   return (
     <div className="App">
-     
-   
-      <PrimarySearchAppBar/>
       <Routes>
+      <Route path="/" exact element={<LoginPage/>}/> 
+      <Route path="/home" exact element={<MiniDrawer/>}/> 
       <Route path="/form" exact element={<Form/>}/>
       </Routes>
      
